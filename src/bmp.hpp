ec5c2888806af9eb2be98a13fa4d5924aa0297fe
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-08-19 11:33:59 +0100

    Test cases for pixelbasher + fixes in src
    When using 'compare_regressions' the colours like red, blue and green where not tracked, so added the appropriate member variables and get/setter methods.
    
    Signed-off-by: elliotfreebairn <elliot.freebairn@collabora.com>

--- src/bmp.hpp
+++ src/bmp.hpp
@@ -67,11 +67,15 @@ public:
     int get_height() const { return m_info_header.height; }
     int get_red_count() const { return m_red_count; }
     int get_yellow_count() const { return m_yellow_count; }
+    int get_blue_count() const { return m_blue_count; }
+    int get_green_count() const { return m_green_count; }
     int get_background_value() const;
     int get_non_background_pixel_count() const;
 
     void increment_red_count(int new_red) { m_red_count += new_red; }
     void increment_yellow_count(int new_yellow) { m_yellow_count += new_yellow; }
+    void increment_blue_count(int new_blue) { m_blue_count += new_blue; }
+    void increment_green_count(int new_green) { m_green_count += new_green; }
     void set_data(std::vector<std::uint8_t> &new_data);
 
 private:
@@ -96,6 +100,8 @@ private:
     std::vector<std::uint8_t> m_data;
     int m_red_count = 0;
     int m_yellow_count = 0;
+    int m_blue_count = 0;
+    int m_green_count = 0;
     std::vector<bool> m_blurred_edge_mask;
     std::vector<bool> m_filtered_edge_mask;
 };
---
+++ test/test_pixelbasher.cpp
@@ -0,0 +1,64 @@
+#include <catch2/catch_all.hpp>
+
+#include "../src/pixelbasher.hpp"
+
+#include <bit>
+#include <iostream>
+
+using Catch::Matchers::ContainsSubstring;
+
+TEST_CASE("PixelBasher compare two BMP's", "[pixelbasher][compare]") {
+    SECTION("Compare two different BMPs and generate a diff image") {
+        BMP original("test_data/solid_white.bmp");
+        BMP target("test_data/vertical_edges.bmp");
+
+        BMP diff = PixelBasher::compare_bmps(original, target, false);
+
+        REQUIRE(diff.get_width() <= original.get_width());
+        REQUIRE(diff.get_height() <= original.get_height());
+        REQUIRE(diff.get_red_count() > 0);
+    }
+
+    SECTION("Compare identical BMPs and generate a diff image") {
+        BMP original("test_data/solid_white.bmp");
+        BMP target("test_data/solid_white.bmp");
+
+        BMP diff = PixelBasher::compare_bmps(original, target, false);
+
+        REQUIRE(diff.get_width() <= original.get_width());
+        REQUIRE(diff.get_height() <= original.get_height());
+        REQUIRE(diff.get_red_count() == 0);
+    }
+}
+
+TEST_CASE("PixelBasher compare regression", "[pixelbasher][regression]") {
+    SECTION("Compare differences with regressed BMP version versus previous regressed BMP version") {
+        BMP original("test_data/solid_white.bmp");
+        BMP current("test_data/solid_black.bmp");
+        BMP previous("test_data/vertical_edges.bmp");
+
+        BMP current_diff = PixelBasher::compare_bmps(original, current, false);
+        BMP previous_diff = PixelBasher::compare_bmps(original, previous, false);
+        BMP diff = PixelBasher::compare_regressions(original, current_diff, previous_diff   );
+
+        REQUIRE(diff.get_width() <= original.get_width());
+        REQUIRE(diff.get_height() <= original.get_height());
+        REQUIRE(diff.get_red_count() > 0);
+        REQUIRE(diff.get_blue_count() > 0);
+    }
+
+    SECTION("Compare differences with fixed BMP version versus regressed BMP version") {
+        BMP original("test_data/solid_white.bmp");
+        BMP current("test_data/solid_white.bmp");
+        BMP previous("test_data/solid_black.bmp");
+
+        BMP current_diff = PixelBasher::compare_bmps(original, current, false);
+        BMP previous_diff = PixelBasher::compare_bmps(original, previous, false);
+        BMP diff = PixelBasher::compare_regressions(original, current_diff, previous_diff);
+
+        int diff_size = diff.get_width() * diff.get_height();
+        REQUIRE(diff.get_green_count() == diff_size);
+        REQUIRE(diff.get_red_count() == 0);
+        REQUIRE(diff.get_blue_count() == 0);
+    }
+}
